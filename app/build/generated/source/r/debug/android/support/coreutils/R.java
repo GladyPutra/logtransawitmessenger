f862a7dc862414829a0dbdb47f86957131227598
Agustinus Gladyawan Dwi Putra <45622674+GladyPutra@users.noreply.github.com>
2019-01-31 10:33:35 +0700

    Fix Version

--- app/build/generated/source/r/debug/android/support/coreutils/R.java
+++ app/build/generated/source/r/debug/android/support/coreutils/R.java
@@ -71,39 +71,39 @@ public final class R {
         public static final int actions = 0x7f080018;
         public static final int async = 0x7f080020;
         public static final int blocking = 0x7f080025;
-        public static final int chronometer = 0x7f080031;
-        public static final int forever = 0x7f080052;
-        public static final int icon = 0x7f08005a;
-        public static final int icon_group = 0x7f08005b;
-        public static final int info = 0x7f08005f;
-        public static final int italic = 0x7f080061;
-        public static final int line1 = 0x7f080066;
-        public static final int line3 = 0x7f080067;
-        public static final int normal = 0x7f080075;
-        public static final int notification_background = 0x7f080076;
-        public static final int notification_main_column = 0x7f080077;
-        public static final int notification_main_column_container = 0x7f080078;
-        public static final int right_icon = 0x7f080088;
-        public static final int right_side = 0x7f080089;
-        public static final int tag_transition_group = 0x7f0800b5;
-        public static final int text = 0x7f0800b6;
-        public static final int text2 = 0x7f0800b7;
-        public static final int time = 0x7f0800be;
-        public static final int title = 0x7f0800bf;
+        public static final int chronometer = 0x7f080032;
+        public static final int forever = 0x7f080053;
+        public static final int icon = 0x7f08005c;
+        public static final int icon_group = 0x7f08005d;
+        public static final int info = 0x7f080063;
+        public static final int italic = 0x7f080065;
+        public static final int line1 = 0x7f08006b;
+        public static final int line3 = 0x7f08006c;
+        public static final int normal = 0x7f08007a;
+        public static final int notification_background = 0x7f08007b;
+        public static final int notification_main_column = 0x7f08007c;
+        public static final int notification_main_column_container = 0x7f08007d;
+        public static final int right_icon = 0x7f08008d;
+        public static final int right_side = 0x7f08008e;
+        public static final int tag_transition_group = 0x7f0800bc;
+        public static final int text = 0x7f0800bd;
+        public static final int text2 = 0x7f0800be;
+        public static final int time = 0x7f0800c5;
+        public static final int title = 0x7f0800c6;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f09000a;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a0034;
-        public static final int notification_action_tombstone = 0x7f0a0035;
-        public static final int notification_template_custom_big = 0x7f0a003c;
-        public static final int notification_template_icon_group = 0x7f0a003d;
-        public static final int notification_template_part_chronometer = 0x7f0a0041;
-        public static final int notification_template_part_time = 0x7f0a0042;
+        public static final int notification_action = 0x7f0a0035;
+        public static final int notification_action_tombstone = 0x7f0a0036;
+        public static final int notification_template_custom_big = 0x7f0a003d;
+        public static final int notification_template_icon_group = 0x7f0a003e;
+        public static final int notification_template_part_chronometer = 0x7f0a0042;
+        public static final int notification_template_part_time = 0x7f0a0043;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0d0042;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0043;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0e00f2;
